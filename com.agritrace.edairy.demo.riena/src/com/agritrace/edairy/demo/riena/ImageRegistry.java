b397f901293e04ac08d821535e2c1691d225eed8
Bill Jones <bill@averline.com>
2010-03-28 18:23:27 +0300

    added real photo for staff view

--- com.agritrace.edairy.demo.riena/src/com/agritrace/edairy/demo/riena/ImageRegistry.java
+++ com.agritrace.edairy.demo.riena/src/com/agritrace/edairy/demo/riena/ImageRegistry.java
@@ -2,7 +2,7 @@ package com.agritrace.edairy.demo.riena;
 
 public class ImageRegistry {
 	
-	public static String smileFace = "icons/smileFace.gif";
+	public static String smileFace = "resources/manager-small.png";
 	
 	public static String search = "icons/insp_sbook.gif";
 	
