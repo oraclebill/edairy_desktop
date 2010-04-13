ca823fc3ed8de864e8456f7b2fde52195538cd71
Bill Jones <oraclebill@gmail.com>
2010-04-13 19:41:42 -0400

    renames, move and plugin.xml cleanup.

---
+++ com.agritrace.edairy.desktop/src/com/agritrace/edairy/desktop/application/ImageRegistry.java
@@ -0,0 +1,10 @@
+package com.agritrace.edairy.desktop;
+
+public class ImageRegistry {
+	
+	public static String smileFace = "resources/manager-small.png";
+	
+	public static String search = "icons/insp_sbook.gif";
+	
+
+}
