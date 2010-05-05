3ab842ac1ebaa40e649e44c9f0264e51fed9eb57
fsli <musicianli@hotmail.com>
2010-05-06 00:12:25 +0800

    Add getters/setters for DairyLocation

--- com.agritrace.edairy.common/src/com/agritrace/edairy/common/datamodel/dairy/DairyLocation.java
+++ com.agritrace.edairy.common/src/com/agritrace/edairy/common/datamodel/dairy/DairyLocation.java
@@ -19,5 +19,61 @@ public class DairyLocation {
 	private String phoneNumber;	
 	private Location location;	
 	private String description;
-	private DairyFunction function; 
+	private DairyFunction function;
+	public Long getCollectionCentreId() {
+		return collectionCentreId;
+	}
+	public void setCollectionCentreId(Long collectionCentreId) {
+		this.collectionCentreId = collectionCentreId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDateOpened() {
+		return dateOpened;
+	}
+	public void setDateOpened(Date dateOpened) {
+		this.dateOpened = dateOpened;
+	}
+	public Route getRoute() {
+		return route;
+	}
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+	public String getColorCode() {
+		return colorCode;
+	}
+	public void setColorCode(String colorCode) {
+		this.colorCode = colorCode;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public DairyFunction getFunction() {
+		return function;
+	}
+	public void setFunction(DairyFunction function) {
+		this.function = function;
+	} 
+	
+	
 }
