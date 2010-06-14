2a480749c97f1ecbec5111aec5ca3d0f31720386
Bill Jones <bill@averline.com>
2010-06-14 16:43:25 -0400

    fixed binding issue w/employee editor

--- com.agritrace.edairy.desktop.operations.ui/src/com/agritrace/edairy/desktop/operations/ui/dialogs/EmployeeBindingConstants.java
+++ com.agritrace.edairy.desktop.operations.ui/src/com/agritrace/edairy/desktop/operations/ui/dialogs/EmployeeBindingConstants.java
@@ -2,10 +2,10 @@ package com.agritrace.edairy.desktop.operations.ui.dialogs;
 
 public interface EmployeeBindingConstants {
 
-	String BIND_ID_EMPLOYEE_ID = null;
-	String BIND_ID_FAMILY_NAME = null;
-	String BIND_ID_GIVEN_NAME = null;
-	String BIND_ID_DEPARTMENT = null;
-	String BIND_ID_POSITION = null;
+	String BIND_ID_EMPLOYEE_ID = "BIND_ID_EMPLOYEE_ID";
+	String BIND_ID_FAMILY_NAME = "BIND_ID_FAMILY_NAME";
+	String BIND_ID_GIVEN_NAME = "BIND_ID_GIVEN_NAME";
+	String BIND_ID_DEPARTMENT = "BIND_ID_DEPARTMENT";
+	String BIND_ID_POSITION = "BIND_ID_POSITION";
 
 }
