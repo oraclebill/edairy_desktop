543a3939e22a909ac0f4668fbdb709b817b2357a
Bill Jones <bill@averline.com>
2010-06-14 20:23:13 -0400

    initial addcollectionjournal page dialog

---
+++ com.agritrace.edairy.desktop.collection.ui/src/com/agritrace/edairy/desktop/collection/ui/NewMilkCollectionJournalDialog.java
@@ -0,0 +1,164 @@
+package com.agritrace.edairy.desktop.collection.ui;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.eclipse.core.databinding.observable.Observables;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.emf.databinding.EMFObservables;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.riena.ui.ridgets.IComboRidget;
+import org.eclipse.riena.ui.ridgets.IDateTimeRidget;
+import org.eclipse.riena.ui.ridgets.swt.SwtRidgetFactory;
+import org.eclipse.riena.ui.swt.DatePickerComposite;
+import org.eclipse.riena.ui.swt.utils.UIControlsFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import com.agritrace.edairy.desktop.common.model.dairy.CollectionJournalPage;
+import com.agritrace.edairy.desktop.common.model.dairy.DairyFactory;
+import com.agritrace.edairy.desktop.common.model.dairy.DairyPackage;
+import com.agritrace.edairy.desktop.common.model.dairy.Employee;
+import com.agritrace.edairy.desktop.common.model.dairy.Route;
+import com.agritrace.edairy.desktop.common.model.dairy.Session;
+import com.agritrace.edairy.desktop.common.model.dairy.Vehicle;
+import com.agritrace.edairy.desktop.member.ui.views.EMFObjectUtil;
+import com.agritrace.edairy.desktop.operations.services.DairyRepository;
+
+public class NewMilkCollectionJournalDialog extends TitleAreaDialog {
+
+	private DatePickerComposite datePicker;
+	private CCombo routeCombo;
+	private CCombo vehicleCombo;
+	private CCombo sessionCombo;
+	private CCombo driverCombo;
+	
+	private final DairyRepository dairyRepository = new DairyRepository(); 
+	private final CollectionJournalPage newJournalPage = DairyFactory.eINSTANCE.createCollectionJournalPage();
+
+	public NewMilkCollectionJournalDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		final Control contents = super.createContents(parent);
+		setTitle("Create New Journal Book");
+		setMessage("Please enter the details for the new Journal Book.");
+		return contents;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite buffer = (Composite) super.createDialogArea(parent);
+		Composite workArea = UIControlsFactory.createComposite(buffer);
+		workArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		// workArea.setLayout(new GridLayout(3, true));
+		{
+			Label label = UIControlsFactory.createLabel(workArea, "Date");
+			datePicker = UIControlsFactory.createDatePickerComposite(workArea, "date-picker");
+			((GridData) datePicker.getTextfield().getLayoutData()).grabExcessVerticalSpace = false;
+			Label filler = UIControlsFactory.createLabel(workArea, "");
+		}
+		{
+			Label label = UIControlsFactory.createLabel(workArea, "Route");
+			routeCombo = UIControlsFactory.createCCombo(workArea, "route");
+			Label filler = UIControlsFactory.createLabel(workArea, "");
+		}
+		{
+			Label label = UIControlsFactory.createLabel(workArea, "Vehicle");
+			vehicleCombo = UIControlsFactory.createCCombo(workArea, "vehicle");
+			Label filler = UIControlsFactory.createLabel(workArea, "");
+		}
+		{
+			Label label = UIControlsFactory.createLabel(workArea, "Session");
+			sessionCombo = UIControlsFactory.createCCombo(workArea, "sesison");
+			Label filler = UIControlsFactory.createLabel(workArea, "");
+		}
+		{
+			Label label = UIControlsFactory.createLabel(workArea, "Driver");
+			driverCombo = UIControlsFactory.createCCombo(workArea, "driver");
+			Label filler = UIControlsFactory.createLabel(workArea, "");
+		}
+		GridLayoutFactory.swtDefaults().numColumns(3).equalWidth(true).generateLayout(workArea);
+		GridDataFactory.swtDefaults().grab(true, true).align(SWT.FILL, SWT.FILL);
+
+		configureRidgets();
+		bindRidgets();
+
+		return buffer;
+	}
+
+	/**
+	 * Creates the buttons for the button bar
+	 * 
+	 * @param parent
+	 *            the parent composite
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		final Button okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		okButton.setEnabled(false);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, true);
+
+	}
+
+	@Override
+	protected Control createButtonBar(Composite parent) {
+		// TODO Auto-generated method stub
+		return super.createButtonBar(parent);
+	}
+
+	private void configureRidgets() {
+		// crate ridgets
+		IDateTimeRidget dateTime = (IDateTimeRidget)SwtRidgetFactory.createRidget(datePicker);
+		IComboRidget route = (IComboRidget)SwtRidgetFactory.createRidget(routeCombo);
+		IComboRidget vehicle = (IComboRidget)SwtRidgetFactory.createRidget(vehicleCombo);
+		IComboRidget session = (IComboRidget)SwtRidgetFactory.createRidget(sessionCombo);
+		IComboRidget driver = (IComboRidget)SwtRidgetFactory.createRidget(driverCombo);
+		
+		// bind date
+		dateTime.bindToModel(EMFObservables.observeValue(newJournalPage, DairyPackage.Literals.COLLECTION_JOURNAL_PAGE__JOURNAL_DATE));
+		dateTime.setDate(new Date());
+		
+		// bind route
+		route.bindToModel(new WritableList(dairyRepository.getRoutes(), Route.class), 
+				Route.class, "getName", 
+				EMFObservables.observeValue(newJournalPage, DairyPackage.Literals.COLLECTION_JOURNAL_PAGE__ROUTE));
+		route.setMandatory(true);
+		
+		// bind vehicle
+		vehicle.bindToModel(new WritableList(dairyRepository.getVehicles(), Vehicle.class), 
+				Vehicle.class, "getLogBookNumber", 
+				EMFObservables.observeValue(newJournalPage, DairyPackage.Literals.COLLECTION_JOURNAL_PAGE__VEHICLE));
+		vehicle.setMandatory(true);
+		
+		// bind session
+		session.bindToModel(Observables.staticObservableList(Arrays.asList(Session.values(), Session.class)), 
+				Session.class, "getName", 
+				EMFObservables.observeValue(newJournalPage, DairyPackage.Literals.COLLECTION_JOURNAL_PAGE__SESSION));
+		session.setMandatory(true);
+		
+		// bind driver
+		driver.bindToModel(new WritableList(dairyRepository.getEmployees("Driver"), Employee.class), 
+				Employee.class, "getFamilyName", 
+				EMFObservables.observeValue(newJournalPage, DairyPackage.Literals.COLLECTION_JOURNAL_PAGE__DRIVER));
+		driver.setMandatory(true);
+		
+		
+	}
+	
+	private void bindRidgets() {
+		
+	}
+
+}
