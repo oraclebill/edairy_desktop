173301d0ca8a6dd9534370a6d6084823ce261eb5
unknown <bjones@.(none)>
2010-06-25 22:14:24 -0400

    made activator private in persistence; read hibernate properties on start

--- com.agritrace.edairy.desktop.common.persistence/src/com/agritrace/edairy/desktop/common/persistence/services/PersistenceManager.java
+++ com.agritrace.edairy.desktop.common.persistence/src/com/agritrace/edairy/desktop/common/persistence/services/PersistenceManager.java
@@ -1,15 +1,26 @@
 package com.agritrace.edairy.desktop.common.persistence.services;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 import java.util.Properties;
+import java.util.logging.Level;
 
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.teneo.hibernate.HbDataStore;
 import org.eclipse.emf.teneo.hibernate.HbHelper;
+import org.eclipse.riena.core.Log4r;
+import org.eclipse.riena.core.RienaLocations;
 import org.eclipse.riena.core.RienaStatus;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Environment;
 
+import  com.agritrace.edairy.desktop.internal.common.persistence.Activator;
 import com.agritrace.edairy.desktop.common.model.base.ModelPackage;
 import com.agritrace.edairy.desktop.common.model.dairy.DairyPackage;
 import com.agritrace.edairy.desktop.common.model.dairy.account.AccountPackage;
@@ -17,8 +28,12 @@ import com.agritrace.edairy.desktop.common.model.requests.RequestsPackage;
 import com.agritrace.edairy.desktop.common.model.tracking.TrackingPackage;
 
 public class PersistenceManager {
+
+	private static final org.eclipse.equinox.log.Logger LOG = 
+		Log4r.getLogger(Activator.getDefault(),PersistenceManager.class);
 	
 	public static final String DB_NAME = "dairytest";
+	public static final String PROPERTIES_FILE_NAME = "edairydb.properties";
 	
 	private static PersistenceManager INSTANCE;
 	
@@ -74,7 +89,28 @@ public class PersistenceManager {
 		// TODO: hibernate properties can be set by having a hibernate.properties file in the root of
 		// the classpath.
 		final Properties props = new Properties();
+
+		final File hibernateProperties = new File(RienaLocations.getDataArea(), PROPERTIES_FILE_NAME);
+		System.err.println( "looking for hibernate properties in '" + hibernateProperties + "'");
+		if (hibernateProperties.canRead()) {
+			System.err.println( "reading hibernate properties from properties file");
+			try {
+				props.load(new FileInputStream(hibernateProperties));
+				return props;
+			} catch (Exception e) {
+				e.printStackTrace();
+				LOG.log(0, e.getMessage(), e);
+			}
+		}
 		
+		System.err.println( "reading hibernate properties detaults");
+
+//		props.setProperty(Environment.DRIVER, "org.hsqldb.jdbcDriver");
+//		props.setProperty(Environment.USER, "SA");
+//		props.setProperty(Environment.URL, "jdbc:hsqldb:file:\\edairydev.db");
+//		props.setProperty(Environment.PASS, "");
+//		props.setProperty(Environment.DIALECT, "org.hibernate.dialect.HSQLDialect");
+
 		props.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
 		props.setProperty(Environment.USER, "root");
 		props.setProperty(Environment.URL, "jdbc:mysql://127.0.0.1:3306/" + DB_NAME);
@@ -104,7 +140,21 @@ public class PersistenceManager {
 	
 	protected void postInit() {
 		System.err.println( ">>>>>> PersistenceManager[" + getClass().getName() + ":" + hashCode() + "] started on thread " + Thread.currentThread());
-		System.err.println(hbds.getMappingXML()); 
+//		System.err.println(hbds.getMappingXML()); 
+//		System.err.println( hbds.getProperties() );
+		File propFile = new File(RienaLocations.getDataArea(), PROPERTIES_FILE_NAME);
+		if (!propFile.exists()) {
+			System.err.println("Saving properties to " + propFile);
+			try {
+				hbds.getProperties().store(new FileOutputStream(propFile), "default properties, written on " + new Date());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
---
+++ com.agritrace.edairy.desktop.common.persistence/src/com/agritrace/edairy/desktop/internal/common/persistence/Activator.java
@@ -0,0 +1,25 @@
+package com.agritrace.edairy.desktop.internal.common.persistence;
+
+import org.eclipse.riena.core.RienaActivator;
+import org.eclipse.riena.core.RienaPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends RienaActivator {
+	private static Activator DEFAULT = null;
+	
+	public static Activator getDefault() {
+		return DEFAULT;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		DEFAULT = this;
+	}
+	
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		DEFAULT = null;
+	}
+}
