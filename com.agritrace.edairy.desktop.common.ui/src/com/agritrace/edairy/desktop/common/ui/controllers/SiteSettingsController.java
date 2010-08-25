e6126e5c50f954a565a97d9989c66ec94ee2d28d
Matvey Kozhev <inetperson@gmail.com>
2010-08-25 13:14:35 +0700

    Rename user preferences to site preferences; refactor to use IPreferenceStore (lets us automatically generate the UI)

---
+++ com.agritrace.edairy.desktop.common.ui/src/com/agritrace/edairy/desktop/common/ui/controllers/SiteSettingsController.java
@@ -0,0 +1,80 @@
+package com.agritrace.edairy.desktop.common.ui.controllers;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceStore;
+import org.hibernate.cfg.Environment;
+
+import com.agritrace.edairy.desktop.common.persistence.services.PersistenceManager;
+
+public class SiteSettingsController {
+	public static final String DB_HOST = "SiteSettingsController.Custom.DB_HOST";
+	public static final String DB_NAME = "SiteSettingsController.Custom.DB_NAME";
+	
+	private static PreferenceStore loadPreferenceStore(Properties props) {
+		final PreferenceStore prefs = new PreferenceStore();
+		
+		for (final Enumeration<?> e = props.propertyNames(); e.hasMoreElements(); ) {
+			String key = e.nextElement().toString();
+			
+			if (!key.equals(Environment.URL))
+				prefs.putValue(key, props.getProperty(key));
+		}
+
+		// JDBC URL requires special treatment
+		final Matcher matcher = Pattern.compile("jdbc:mysql://(.*)/(.*)").matcher(props.getProperty(Environment.URL));
+		matcher.matches();
+		prefs.putValue(DB_HOST, matcher.group(1));
+		prefs.putValue(DB_NAME, matcher.group(2));
+		
+		return prefs;
+	}
+		
+	private static void dumpPreferenceStore(PreferenceStore prefs, Properties props) {
+		for (final String key: prefs.preferenceNames()) {
+			if (!key.equals(DB_HOST) && !key.equals(DB_NAME))
+				props.setProperty(key, prefs.getString(key));
+		}
+		
+		// JDBC URL requires special treatment
+		final String url = String.format("jdbc:mysql://%s/%s", prefs.getString(DB_HOST), prefs.getString(DB_NAME));
+		props.setProperty(Environment.URL, url);
+		System.out.println(String.format("URL: %s, property: %s", url, props.getProperty(Environment.URL)));
+	}
+	
+	private Properties props;
+	private PreferenceStore prefs;
+
+	/**
+	 * Load the properties.
+	 * 
+	 * @return Preference store to use in a preference page
+	 */
+	public IPreferenceStore loadData() {
+		props = PersistenceManager.getDefault().getProperties();
+		prefs = loadPreferenceStore(props);
+		return prefs;
+	}
+	
+	/**
+	 * Save the properties.
+	 * 
+	 * @return Whether the database properties have been modified since last change.
+	 * @throws IOException
+	 */
+	public boolean saveData() throws IOException {
+		Properties propsCopy = (Properties) props.clone();
+		dumpPreferenceStore(prefs, props);
+		
+		if (propsCopy.equals(props))
+			return false;
+		
+		PersistenceManager.getDefault().saveProperties(props);
+		return true;
+	}
+}
--- com.agritrace.edairy.desktop.common.ui/src/com/agritrace/edairy/desktop/common/ui/dialogs/PreferencesDialog.java
+++ com.agritrace.edairy.desktop.common.ui/src/com/agritrace/edairy/desktop/common/ui/dialogs/PreferencesDialog.java
@@ -3,20 +3,22 @@ package com.agritrace.edairy.desktop.common.ui.dialogs;
 import java.io.IOException;
 
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferenceManager;
 import org.eclipse.jface.preference.PreferenceNode;
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+import org.hibernate.cfg.Environment;
 
 import com.agritrace.edairy.desktop.common.ui.controllers.SystemSettingsController;
-import com.agritrace.edairy.desktop.common.ui.controllers.UserSettingsController;
+import com.agritrace.edairy.desktop.common.ui.controllers.SiteSettingsController;
 
 /**
  * Preferences dialog invoked from the Preference menu, with two pages.
@@ -25,26 +27,13 @@ import com.agritrace.edairy.desktop.common.ui.controllers.UserSettingsController
  *
  */
 public final class PreferencesDialog extends PreferenceDialog {
-	private static final class UserPrefsPage extends PreferencePage {
-		private UserSettingsController controller;
+	private static final class SitePrefsPage extends FieldEditorPreferencePage {
+		private SiteSettingsController controller;
 		
-		public UserPrefsPage() {
-			super("User preferences");
+		public SitePrefsPage() {
+			super("Site preferences", GRID);
 			setDescription("Settings that affect the client installation.");
-			controller = new UserSettingsController();
-		}
-		
-		@Override
-		protected Control createContents(Composite parent) {
-			final Composite area = new Composite(parent, SWT.NONE);
-			GridDataFactory factory = GridDataFactory.swtDefaults().grab(true, false);
-			area.setLayout(new GridLayout(2, false));
-			factory.applyTo(area);
-			
-			controller.addControls(area);
-			controller.loadData();
-			
-			return area;
+			controller = new SiteSettingsController();
 		}
 		
 		@Override
@@ -56,6 +45,9 @@ public final class PreferencesDialog extends PreferenceDialog {
 		@Override
 		public boolean performOk() {
 			try {
+				// Update preference store before saving
+				super.performOk();
+				
 				if (controller.saveData()) {
 					MessageDialog dlg = new MessageDialog(this.getControl().getShell(), "Settings saved", null,
 							"The user settings have been saved. The new database settings will not be used " +
@@ -69,8 +61,26 @@ public final class PreferencesDialog extends PreferenceDialog {
 				
 				return true;
 			} catch (IOException e) {
+				MessageDialog.openError(this.getControl().getShell(), "Error", e.getMessage());
 				return false;
 			}
+		}
+		
+		@Override
+		public boolean performCancel() {
+			// Suppress default cancel action, do nothing
+			return true;
+		}
+
+		@Override
+		protected void createFieldEditors() {
+			setPreferenceStore(controller.loadData());
+			Composite comp = getFieldEditorParent();
+			
+			addField(new StringFieldEditor(SiteSettingsController.DB_HOST, "Database host:", comp));
+			addField(new StringFieldEditor(SiteSettingsController.DB_NAME, "Database name:", comp));
+			addField(new StringFieldEditor(Environment.USER, "Database user:", comp));
+			addField(new StringFieldEditor(Environment.PASS, "Database password:", comp));
 		}
 	}
 	
@@ -111,7 +121,7 @@ public final class PreferencesDialog extends PreferenceDialog {
 	
 	private static PreferenceManager createPreferenceManager() {
 		PreferenceManager manager = new PreferenceManager();
-		manager.addToRoot(new PreferenceNode("user", new UserPrefsPage()));
+		manager.addToRoot(new PreferenceNode("user", new SitePrefsPage()));
 		manager.addToRoot(new PreferenceNode("system", new SystemPrefsPage()));
 		return manager;
 	}
