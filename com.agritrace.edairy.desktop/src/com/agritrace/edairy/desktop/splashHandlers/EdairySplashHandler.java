bae65169b4e2e34e26a432b5443ce1c9deca2cb6
Matvey Kozhev <inetperson@gmail.com>
2010-08-25 18:32:20 +0700

    Add second splash screen

---
+++ com.agritrace.edairy.desktop/src/com/agritrace/edairy/desktop/splashHandlers/EdairySplashHandler.java
@@ -0,0 +1,37 @@
+package com.agritrace.edairy.desktop.splashHandlers;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.internal.splash.EclipseSplashHandler;
+import org.eclipse.ui.splash.BasicSplashHandler;
+
+import com.agritrace.edairy.desktop.member.ui.Activator;
+
+@SuppressWarnings("restriction")
+public class EdairySplashHandler extends EclipseSplashHandler {
+	/*
+	@Override
+	public void init(Shell splash) {
+		super.init(splash);
+	}
+	*/
+	
+	@Override
+	protected Composite getContent() {
+		Composite monitor = super.getContent();
+		Composite parent = monitor.getParent();
+		
+		// getSplash()
+		// getSplash().setBackgroundImage(null);
+		// getSplash().setBackgroundMode(SWT.INHERIT_NONE);
+		//parent.setBackgroundImage(Activator.getImage("resources/splash/splash.bmp"));
+		// parent.setBackground(new Color(parent.getDisplay(), 255, 128, 128));
+		//parent.setBackgroundMode(SWT.INHERIT_FORCE);
+		monitor.setBackgroundImage(Activator.getImage("splash/splash.bmp"));
+		
+		return monitor;
+	}
+}
