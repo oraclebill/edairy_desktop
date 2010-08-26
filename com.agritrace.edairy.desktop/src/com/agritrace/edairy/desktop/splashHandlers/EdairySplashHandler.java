ef827e7c2cf1800d8df3262d6776914419993685
Matvey Kozhev <inetperson@gmail.com>
2010-08-26 12:25:00 +0700

    Add Help menu, change splash screen to load sooner

--- com.agritrace.edairy.desktop/src/com/agritrace/edairy/desktop/splashHandlers/EdairySplashHandler.java
+++ com.agritrace.edairy.desktop/src/com/agritrace/edairy/desktop/splashHandlers/EdairySplashHandler.java
@@ -1,7 +1,9 @@
 package com.agritrace.edairy.desktop.splashHandlers;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.internal.StartupThreading.StartupRunnable;
 import org.eclipse.ui.internal.splash.EclipseSplashHandler;
 
 import com.agritrace.edairy.desktop.common.persistence.services.PersistenceManager;
@@ -12,13 +14,18 @@ public class EdairySplashHandler extends EclipseSplashHandler {
 	@Override
 	public void init(Shell splash) {
 		super.init(splash);
-
-		getBundleProgressMonitor().beginTask("Initializing database", 1);
-		getSplash().getDisplay().asyncExec(new Runnable() {
+		
+		final IProgressMonitor monitor = getBundleProgressMonitor();
+		monitor.beginTask("Initializing database", 2);
+		
+		getSplash().getDisplay().asyncExec(new StartupRunnable() {
 			@Override
-			public void run() {
-				PersistenceManager.getDefault().getSession().close();
-				getBundleProgressMonitor().worked(1);
+			public void runWithException() throws Throwable {
+				PersistenceManager pm = PersistenceManager.getDefault();
+				monitor.worked(1);
+				// Force Hibernate to initialize - how should we do this elegantly?
+				pm.getSession().close();
+				monitor.worked(1);
 			}
 		});
 	}
