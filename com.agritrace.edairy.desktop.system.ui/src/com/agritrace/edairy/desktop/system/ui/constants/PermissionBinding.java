4a11501934d4b9d636e97667f44447b1dd8a0e8e
Matvey Kozhev <inetperson@gmail.com>
2010-09-06 10:23:50 +0700

    Delete dynamic permission edit code (can be restored from git if needed)

--- com.agritrace.edairy.desktop.system.ui/src/com/agritrace/edairy/desktop/system/ui/constants/PermissionBinding.java
+++
@@ -1,9 +0,0 @@
-package com.agritrace.edairy.desktop.system.ui.constants;
-
-public enum PermissionBinding {
-	BIND_ID_PERMISSION_ID,
-	BIND_ID_PERMISSION_NAMESPACE_NAME,
-	BIND_ID_PERMISSION_NAME,
-	BIND_ID_PERMISSION_DISPLAYNAME,
-	NAMESPACE_LOOKUP_BUTTON
-}
