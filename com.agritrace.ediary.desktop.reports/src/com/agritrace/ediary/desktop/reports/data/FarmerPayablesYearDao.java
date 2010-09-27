40647de63936922154a649cfe34255b3faae607b
Bill Jones <bill@averline.com>
2010-09-26 21:21:25 -0400

    updated report dao to use new query methods

--- com.agritrace.ediary.desktop.reports/src/com/agritrace/ediary/desktop/reports/data/FarmerPayablesYearDao.java
+++ com.agritrace.ediary.desktop.reports/src/com/agritrace/ediary/desktop/reports/data/FarmerPayablesYearDao.java
@@ -1,137 +1,190 @@
 package com.agritrace.ediary.desktop.reports.data;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import com.agritrace.edairy.desktop.collection.services.ICollectionJournalLineRepository;
 import com.agritrace.edairy.desktop.common.model.dairy.Membership;
+import com.agritrace.edairy.desktop.common.model.dairy.account.AccountTransaction;
+import com.agritrace.edairy.desktop.common.model.dairy.account.Transaction;
+import com.agritrace.edairy.desktop.common.model.dairy.account.TransactionType;
 import com.agritrace.edairy.desktop.common.model.tracking.Farm;
 import com.agritrace.edairy.desktop.common.model.tracking.Farmer;
 import com.agritrace.edairy.desktop.common.persistence.IMemberRepository;
+import com.agritrace.edairy.desktop.common.persistence.ITransactionRepository;
+import com.agritrace.edairy.desktop.common.persistence.RepositoryFactory;
 
 /**
- * class to collect the data needed for the report and to place it into special beans for report presentation.
+ * class to collect the data needed for the report and to place it into special
+ * beans for report presentation.
+ * 
  * @author rsw
- *
+ * 
  */
 public class FarmerPayablesYearDao {
-	
-	private static final BigDecimal ZERO = new BigDecimal(0);
-	
+
+	private static final MathContext MONEY_CONTEXT = new MathContext(2,
+			RoundingMode.HALF_DOWN);
+	private static final BigDecimal ZERO = new BigDecimal(0, MONEY_CONTEXT);
+
 	private static FarmerPayablesYearDao instance;
+	// private final IDairyRepository dairyRepo;
 	private final IMemberRepository memberRepo;
 	private java.text.DecimalFormat floatFormater;
-	
-	public FarmerPayablesYearDao(){
+
+	public FarmerPayablesYearDao() {
 		super();
 		memberRepo = null;  // TODO: fixme
 		this.floatFormater = new java.text.DecimalFormat("#,#00.0#;(#,#00.0#)");
 	}
-	
-	public static FarmerPayablesYearDao instance(){
-		if(instance == null){
+
+	public static FarmerPayablesYearDao instance() {
+		if (instance == null) {
 			instance = new FarmerPayablesYearDao();
 		}
 		return instance;
 	}
-	
-	public List<FarmerPayablesYearData> getReportValues(String year, String month){
-		
+
+	public List<FarmerPayablesYearData> getReportValues(String year,
+			String month) {
+
 		List<Farm> farms = memberRepo.getMemberFarms();
 		List<FarmerPayablesYearData> ret = new ArrayList<FarmerPayablesYearData>();
-		
-		
-		for(Farm farm:farms){
+
+		for (Farm farm : farms) {
 			Membership membership = (Membership) farm.getOwner().eContainer();
-			if(membership == null){
+			if (membership == null) {
 				continue;
 			}
 			Farmer farmer = membership.getMember();
-			
-			
-			//Income = (Total quantity of milk collected - quantity of rejected milk) * Posted Milk Price for that month
-			float income = 34588;//TODO implement!
-			
-			//Credits = 0 - (Sum of all credits attributed to member for current month)
+
+			// Income = (Total quantity of milk collected - quantity of rejected
+			// milk) * Posted Milk Price for that month
+			float income = 34588;// TODO implement!
+
+			// Credits = 0 - (Sum of all credits attributed to member for
+			// current month)
 			float credits = -34;
-			
-			//Adjustments = Sum of all adjustments (debit adds and credit subtracts)
+
+			// Adjustments = Sum of all adjustments (debit adds and credit
+			// subtracts)
 			float adjustments = 0;
-			
-			//Payables = Sum of Income, credits and adjustments
+
+			// Payables = Sum of Income, credits and adjustments
 			float payables = 1;
-			
+
 			String name = farmer.getGivenName();
 			String memberNumber = membership.getMemberNumber();
 			String accountNumber = membership.getAccount().getAccountNumber();
-			
-			FarmerPayablesYearData data = new FarmerPayablesYearData(name, 
-					memberNumber, accountNumber, this.floatFormater.format(income), 
-					this.floatFormater.format(credits), this.floatFormater.format(adjustments), this.floatFormater.format(payables));
+
+			FarmerPayablesYearData data = new FarmerPayablesYearData(name,
+					memberNumber, accountNumber,
+					this.floatFormater.format(income),
+					this.floatFormater.format(credits),
+					this.floatFormater.format(adjustments),
+					this.floatFormater.format(payables));
 			ret.add(data);
 		}
-		
+
 		return ret;
 	}
 
-	public List<FarmerPayablesYearData> getReportValuesX(String year, String month){
-		
+	public List<FarmerPayablesYearData> getReportValuesX(String year,
+			String month) {
+
 		List<Membership> members = memberRepo.all();
-		List<FarmerPayablesYearData> ret = new ArrayList<FarmerPayablesYearData>();		
-		
-		for(Membership membership:members){
-			Farmer farmer = membership.getMember();
-						
-			//Income = (Total quantity of milk collected - quantity of rejected milk) * Posted Milk Price for that month
-			BigDecimal income = calculateMemberMonthlyIncome(membership, month, year);
-			
-			//Credits = 0 - (Sum of all credits attributed to member for current month)
-			BigDecimal credits = calculateMemberMonthlyCredits(membership, month, year);
-			
-			//Adjustments = Sum of all adjustments (debit adds and credit subtracts)
-			BigDecimal adjustments = calculateMemberMonthlyAdjustments(membership, month, year);
-			
-			//Payables = Sum of Income, credits and adjustments
-			BigDecimal payables = calculateMemberMonthlyPayable(membership, month, year);
+		List<FarmerPayablesYearData> ret = new ArrayList<FarmerPayablesYearData>();
 
-			if (income.equals(ZERO) && credits.equals(ZERO) && adjustments.equals(ZERO)) 
+		for (Membership membership : members) {
+			Farmer farmer = membership.getMember();
+
+			// Income = (Total quantity of milk collected - quantity of rejected
+			// milk) * Posted Milk Price for that month
+			BigDecimal income = calculateMemberMonthlyIncome(membership, month,
+					year);
+
+			// Credits = 0 - (Sum of all credits attributed to member for
+			// current month)
+			BigDecimal[] creditsAndAdjustments = calculateMemberMonthlyCreditsAndAdjustments(
+					membership, month, year);
+
+			if (income.equals(ZERO) && creditsAndAdjustments[0].equals(ZERO)
+					&& creditsAndAdjustments[1].equals(ZERO))
 				continue;
-			
+
+			// Payables = Sum of Income, credits and adjustments
+			BigDecimal payables = income.add(creditsAndAdjustments[0]).add(
+					creditsAndAdjustments[1]);
+
 			String name = farmer.getGivenName();
 			String memberNumber = membership.getMemberNumber();
 			String accountNumber = membership.getAccount().getAccountNumber();
-			
-			FarmerPayablesYearData data = new FarmerPayablesYearData(name, 
-					memberNumber, accountNumber, this.floatFormater.format(income), 
-					this.floatFormater.format(credits), this.floatFormater.format(adjustments), this.floatFormater.format(payables));
+
+			FarmerPayablesYearData data = new FarmerPayablesYearData(name,
+					memberNumber, accountNumber,
+					this.floatFormater.format(income),
+					this.floatFormater.format(creditsAndAdjustments[0]),
+					this.floatFormater.format(creditsAndAdjustments[1]),
+					this.floatFormater.format(payables));
 			ret.add(data);
 		}
-		
+
 		return ret;
 	}
 
-	private BigDecimal calculateMemberMonthlyIncome(Membership membership,
-			String month, String year) {
-		return ZERO;
+	private BigDecimal calculateMemberMonthlyIncome(
+			final Membership membership, final String month, final String year) {
+		final ICollectionJournalLineRepository collectionsRepo = RepositoryFactory
+				.getRegisteredRepository(ICollectionJournalLineRepository.class);
+		final int priceMonth = Integer.parseInt(month), priceYear = Integer
+				.parseInt(year);
+		final BigDecimal memberCollections = collectionsRepo
+				.getSumOfPayableDeliveries(membership, priceMonth, priceYear);
+		final BigDecimal paymentRate = collectionsRepo.getMilkPrice(priceMonth,
+				priceYear);
+
+		return paymentRate.multiply(memberCollections, MONEY_CONTEXT);
 	}
 
-	private BigDecimal calculateMemberMonthlyCredits(Membership membership,
-			String month, String year) {
-		// TODO Auto-generated method stub
-		return ZERO;
-	}
+	private BigDecimal[] calculateMemberMonthlyCreditsAndAdjustments(
+			Membership membership, String month, String year) {
+		ITransactionRepository transactionRepo = RepositoryFactory
+				.getRegisteredRepository(ITransactionRepository.class);
+		final int priceMonth = Integer.parseInt(month), priceYear = Integer
+				.parseInt(year);
+		Calendar startDate = Calendar.getInstance(), endDate = Calendar
+				.getInstance();
 
-	private BigDecimal calculateMemberMonthlyAdjustments(Membership membership,
-			String month, String year) {
-		// TODO Auto-generated method stub
-		return ZERO;
-	}
+		startDate.set(Calendar.MONTH, priceMonth);
+		startDate.set(Calendar.YEAR, priceYear);
+		startDate.set(Calendar.DAY_OF_MONTH,
+				startDate.getActualMinimum(Calendar.DAY_OF_MONTH));
 
-	private BigDecimal calculateMemberMonthlyPayable(Membership membership,
-			String month, String year) {
-		// TODO Auto-generated method stub
-		return ZERO;
-	}
+		endDate.set(Calendar.MONTH, priceMonth);
+		endDate.set(Calendar.YEAR, priceYear);
+		startDate.set(Calendar.DAY_OF_MONTH,
+				startDate.getActualMaximum(Calendar.DAY_OF_MONTH));
 
+		List<Transaction> transactions = transactionRepo
+				.accountTransactionsInRange(membership.getAccount(),
+						startDate.getTime(), endDate.getTime());
+		BigDecimal credits = ZERO, adjustments = ZERO;
+		for (Transaction tx : transactions) {
+			BigDecimal amt = tx.getAmount();
+			if (tx.getTransactionType().equals(TransactionType.CREDIT)) {
+				amt = ZERO.subtract(amt);
+			}
+			if (tx instanceof AccountTransaction) {
+				credits = credits.add(amt);
+			} else {
+				adjustments = adjustments.add(amt);
+			}
+		}
+		return new BigDecimal[] { credits, adjustments };
+	}
 
 }
