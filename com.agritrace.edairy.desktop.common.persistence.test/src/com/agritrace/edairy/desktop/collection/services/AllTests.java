eb4a663a7e9199328eded49d2687c286cc28cbaf
Bill Jones <bill@averline.com>
2010-10-02 23:37:34 -0400

    add suite to run all tests, also adds aopalliance depdendency

---
+++ com.agritrace.edairy.desktop.common.persistence.test/src/com/agritrace/edairy/desktop/collection/services/AllTests.java
@@ -0,0 +1,31 @@
+package com.agritrace.edairy.desktop.collection.services;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.agritrace.edairy.desktop.common.persistence.services.PersistenceManagerTest;
+import com.agritrace.edairy.desktop.common.persistence.test.DairyPersistenceTest;
+import com.agritrace.edairy.desktop.common.persistence.test.DairyRepoPerfTest;
+import com.agritrace.edairy.desktop.common.persistence.test.DairyRepositoryTest;
+import com.agritrace.edairy.desktop.common.persistence.test.DialogCancelTest;
+import com.agritrace.edairy.desktop.common.persistence.test.MembershipPerformanceTestCase;
+import com.agritrace.edairy.desktop.common.persistence.test.ModelPersistenceTestCase;
+import com.agritrace.edairy.desktop.internal.common.persistence.TransactionRepositoryTest;
+import com.agritrace.edairy.desktop.internal.common.persistence.TransactionTestCase;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	PersistenceManagerTest.class, 
+	DairyPersistenceTest.class,
+	DairyRepoPerfTest.class, 
+	DairyRepositoryTest.class, 
+	ModelPersistenceTestCase.class, 
+	MembershipPerformanceTestCase.class,
+	MilkCollectionJournalLineQueryTest.class,
+	DialogCancelTest.class, 
+	TransactionRepositoryTest.class, 
+	TransactionTestCase.class
+	})
+public class AllTests {
+
+}
