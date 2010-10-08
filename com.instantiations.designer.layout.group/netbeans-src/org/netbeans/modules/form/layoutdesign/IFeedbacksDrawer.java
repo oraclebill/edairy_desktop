386105f4e90697f854ee0f7011317f8ab4d8d746
bjones <bjones@dingo.(none)>
2010-10-08 01:36:55 -0400

    updated deps, removing cruft

---
+++ com.instantiations.designer.layout.group/netbeans-src/org/netbeans/modules/form/layoutdesign/IFeedbacksDrawer.java
@@ -0,0 +1,14 @@
+package org.netbeans.modules.form.layoutdesign;
+
+/**
+ * Interface between netbeans and GEF for drawing feedbacks. Some methods not used, but needed to compile
+ * netbeans code.
+ * 
+ * @author mitin_aa
+ */
+public interface IFeedbacksDrawer {
+    public static final int BOTH_DIMENSIONS = 2;
+	void drawLine(int x1, int y1, int x2, int y2);
+	void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle);
+	void drawLinkBadge(int x, int y, int dimension);
+}
