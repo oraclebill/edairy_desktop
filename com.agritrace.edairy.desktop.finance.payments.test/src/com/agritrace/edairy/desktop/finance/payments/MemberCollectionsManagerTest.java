6c92e6922b37e805418d7b1a6568a79a1e41a33c
Bill Jones <bill@averline.com>
2010-10-14 20:23:47 -0400

    ckpt

--- com.agritrace.edairy.desktop.finance.payments.test/src/com/agritrace/edairy/desktop/finance/payments/MemberCollectionsManagerTest.java
+++ com.agritrace.edairy.desktop.finance.payments.test/src/com/agritrace/edairy/desktop/finance/payments/MemberCollectionsManagerTest.java
@@ -1,23 +1,24 @@
 package com.agritrace.edairy.desktop.finance.payments;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class MemberCollectionsManagerTest {
 
-//	@Test
+	@Test
 	public void testCalculatePayableDeliveries() throws Exception {
-		throw new UnsupportedOperationException("unimplemented");
+		fail("unimplemented");
 	}
 
-//	@Test
+	@Test
 	public void getMilkPriceForPeriod() throws Exception {
-		// TODO Auto-generated method stub
+		fail("unimplemented");
 	}
 
 	@Test
 	public void getActiveMembers() throws Exception {
 		fail("unimplemented");
-
 	}
 }
