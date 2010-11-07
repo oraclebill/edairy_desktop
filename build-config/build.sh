6564533fa1cd659f8a04bae29fcdac191f74edf1
Bill Jones <bill@averline.com>
2010-11-07 04:36:08 -0500

    update agan

--- build-config/build.sh
+++ build-config/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+workspace_directory=/Users/bjones/Development/Projects/edairy_desktop
 sdkroot=/Users/bjones/Development/Applications/eclipse/currentSDK
 launcher=${sdkroot}/plugins/org.eclipse.equinox.launcher_1.1.0.v20100507.jar
 buildfile=${sdkroot}/plugins/org.eclipse.pde.build_3.6.1.R36x_v20100823/scripts/productBuild/productBuild.xml
@@ -8,6 +9,7 @@ baseLocation=../edairy_desktop_lib/exported_target
 tag=`date "+%Y%m%d%H%M"`
 if [ ${BUILD_TAG} ]
 then
+	workspace_directory=${WORKSPACE}
 	baseLocation=../../edairy_desktop_target_build/lastSuccessful/archive/targetPlatform
 	sdkroot=${baseLocation}
 	tag=${BUILD_TAG}
@@ -16,7 +18,7 @@ fi
 java -jar ${launcher} \
 	 -application org.eclipse.ant.core.antRunner \
 	 -buildfile ${buildfile} \
-	 -Dbuilder=/Users/bjones/Development/Projects/edairy_desktop/build-config \
+	 -Dbuilder=${workspace_directory}/build-config \
 	 -DbaseLocation=${baseLocation} \
 	 -Dbuild.compiler=javac1.6 \
      -DbuildId=dairymgr-${tag}
