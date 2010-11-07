41239778c82f78c0540fc8ec616edb7df7806611
Bill Jones <bill@averline.com>
2010-11-07 04:30:12 -0500

    updated to adjust targetplatorm location

--- build-config/build.sh
+++ build-config/build.sh
@@ -3,9 +3,13 @@
 sdkroot=/Users/bjones/Development/Applications/eclipse/currentSDK
 launcher=${sdkroot}/plugins/org.eclipse.equinox.launcher_1.1.0.v20100507.jar
 buildfile=${sdkroot}/plugins/org.eclipse.pde.build_3.6.1.R36x_v20100823/scripts/productBuild/productBuild.xml
+baseLocation=../edairy_desktop_lib/exported_target
+
 tag=`date "+%Y%m%d%H%M"`
 if [ ${BUILD_TAG} ]
 then
+	baseLocation=../../edairy_desktop_target_build/lastSuccessful/archive/targetPlatform
+	sdkroot=${baseLocation}
 	tag=${BUILD_TAG}
 fi
 
@@ -13,6 +17,7 @@ java -jar ${launcher} \
 	 -application org.eclipse.ant.core.antRunner \
 	 -buildfile ${buildfile} \
 	 -Dbuilder=/Users/bjones/Development/Projects/edairy_desktop/build-config \
+	 -DbaseLocation=${baseLocation} \
 	 -Dbuild.compiler=javac1.6 \
      -DbuildId=dairymgr-${tag}
 
